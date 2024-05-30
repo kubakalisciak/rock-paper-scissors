7163495c5be25e52be7cbd4a74bfaef4eee6c24f
kubakalisciak <jakub.kalisciak@gmail.com>
2024-05-30 20:17:20 +0200

    Finish logic for a single game, not tested
    Finish logic for a single game in an if statement.
    
    Add getHumanChoice() with the prompt() method.

--- script.js
+++ script.js
@@ -1,23 +1,64 @@
 console.log('Hello World!')
 
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice () {
     const rand = Math.floor((Math.random()*3)+ 1)
-    let choice = "";
+    let computerChoice = "";
 
     switch (rand) {
         case 1:
-            choice = "rock"
+            computerChoice = "rock"
             break;
         case 2:
-            choice = "paper"
+            computerChoice = "paper"
             break;
         case 3:
-            choice = "scissors"
+            computerChoice = "scissors"
             break;
 
         }
     
-    return choice;
+    return computerChoice;
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt('Rock, paper or scissors? ')
+    humanChoice = humanChoice.toLowerCase()
+
+    return humanChoice;
 }
 
 console.log(getComputerChoice())
+console.log(getHumanChoice())
+
+function playOnce(computer, human) {
+    if (computer == 'scissors' && human == 'rock') {
+        alert('You win! Rock beats scissors!');
+        humanScore++;
+    }
+    else if (computer == 'scissors' && human == 'paper') {
+        alert('You lose! Scissors beat paper!');
+        computerScore++;
+    }
+    else if (computer == "rock" && human == "paper") {
+        alert('You win! Paper beats rock!');
+        humanScore++;
+    }
+    else if (computer == 'rock' && human == 'scissors') {
+        alert('You lose! Rock beats scissors!');
+        computerScore++;
+    }
+    else if (computer == "paper" && human == "scissors") {
+        alert("You win! Scissors beat paper!")
+        humanScore++;
+    }
+    else if (computer == "paper" && human == "rock") {
+        alert("You lose! Paper beats rock!")
+        humanScore++;
+    }
+    else if (computer == human == "rock" || computer == human == "paper" || computer == human == "scissors") {
+        alert("It's a tie!")
+    }
+}

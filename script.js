af21a6e85e5219268c521f93b250e9b57f439793
kubakalisciak <jakub.kalisciak@gmail.com>
2024-05-31 10:11:44 +0200

    Finish the game
    Finish and test the logic of playOnce().
    
    Change the form of the game from console-based to alert-based.
    
    Make function playGame() that calls playOnce() five times and then
    calculates the score.

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-console.log('Hello World!')
+console.log('Hello World!');
 
 let computerScore = 0;
 let humanScore = 0;
 
 function getComputerChoice () {
-    const rand = Math.floor((Math.random()*3)+ 1)
-    let computerChoice = "";
+    const rand = Math.floor((Math.random() * 3)) + 1;
+    let computerChoice = '';
 
     switch (rand) {
         case 1:
-            computerChoice = "rock"
+            computerChoice = 'rock';
             break;
         case 2:
-            computerChoice = "paper"
+            computerChoice = 'paper';
             break;
         case 3:
-            computerChoice = "scissors"
+            computerChoice = 'scissors';
             break;
 
         }
-    
+    console.log(computerChoice);
     return computerChoice;
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt('Rock, paper or scissors? ')
-    humanChoice = humanChoice.toLowerCase()
+    let humanChoice = prompt('Rock, paper or scissors? ');
+    humanChoice = humanChoice.toLowerCase();
 
     return humanChoice;
 }
 
-console.log(getComputerChoice())
-console.log(getHumanChoice())
-
 function playOnce(computer, human) {
+
     if (computer == 'scissors' && human == 'rock') {
         alert('You win! Rock beats scissors!');
         humanScore++;
@@ -51,14 +49,36 @@ function playOnce(computer, human) {
         computerScore++;
     }
     else if (computer == "paper" && human == "scissors") {
-        alert("You win! Scissors beat paper!")
+        alert("You win! Scissors beat paper!");
         humanScore++;
     }
     else if (computer == "paper" && human == "rock") {
-        alert("You lose! Paper beats rock!")
+        alert("You lose! Paper beats rock!");
         humanScore++;
     }
-    else if (computer == human == "rock" || computer == human == "paper" || computer == human == "scissors") {
+    else if (computer == human) {
         alert("It's a tie!")
     }
 }
+
+function playGame () {
+    playOnce(getComputerChoice(), getHumanChoice());
+    playOnce(getComputerChoice(), getHumanChoice());
+    playOnce(getComputerChoice(), getHumanChoice());
+    playOnce(getComputerChoice(), getHumanChoice());
+    playOnce(getComputerChoice(), getHumanChoice());    
+
+    alert("THE END! Your score will be announced!")
+
+    if (computerScore > humanScore) {
+        alert(`You lose! ${humanScore} : ${computerScore}`);
+    }
+    else if (humanScore > computerScore) {
+        alert(`You win! ${humanScore} : ${computerScore}`);
+    }
+    else {
+        alert(`It's a tie! ${humanScore} : ${computerScore}`);
+    }
+}
+
+playGame();
